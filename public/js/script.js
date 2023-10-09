1ddc6c29b69c2b822e4182773dab8f43a19e6140
AidenRIreland <aidenireland2004@gmail.com>
2023-10-09 16:44:01 -0400

    Fixed Script for the arrow to change properly

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded',function(){
     let service = document.querySelectorAll('.service');
-
+    //Select all the divs with service
     service.forEach(service =>{
         let title = service.querySelector('.stitle');
         let description = service.querySelector('.description')
         let arrow = service.querySelector('.arrow');
-
+        //Declare the stuff inside service ima mess with
         title.addEventListener('click', function(){
+            //If someone clicks the title *aka the H4 with the arrow, change the div 
             if(description.style.display === "none" || !description.style.display){
                 description.style.display = "block";
-                arrow.style.transform = 'rotate(90deg)';
+                arrow.innerHTML = '&#x25BC;';
             }
             else{
                 description.style.display ="none";
-                arrow.style.transform = 'rotate(0deg)';
+                arrow.innerHTML = '&#x25BA;';
             }
         })
     })
